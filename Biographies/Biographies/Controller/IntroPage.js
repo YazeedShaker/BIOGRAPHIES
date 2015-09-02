80ff2950d09710da951ee1b35461bdb8536eafa8
Yazeed Shaker <yazeed.shaker@outlook.com>
2015-09-02 03:38:36 +0200

    Adding Controller and styles

---
+++ Biographies/Biographies/Controller/IntroPage.js
@@ -0,0 +1,16 @@
+﻿var windowH = $(window).height(),
+    documEle = $(document),
+    slideDownPage = $('.slide-page-down'),
+    btns = $(".btn"),
+    animeSpeed = 500;
+
+slideDownPage.css({
+    height: windowH + 'px',
+    top: -windowH + 'px'
+});
+
+btns.on('click', function (e) {
+    if ($(this).hasClass('open')) {
+        slideDownPage.animate({ 'top': 0 }, animeSpeed)
+    }
+});
