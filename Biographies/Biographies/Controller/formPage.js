f52654191c13188b090e37102528ec49f0a7ad5c
Yazeed Shaker <yazeed.shaker@outlook.com>
2015-10-23 22:28:00 +0200

    fadeIn the form

---
+++ Biographies/Biographies/Controller/formPage.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="H:\GitHub\BIOGRAPHIES\Biographies\Biographies\Scripts/jquery-2.1.4.js" />
+
+$(document).ready(function () {
+    formFadeIn();
+    $("label").css("color", "white");
+});
+
+function formFadeIn() {
+    $(".form-horizontal").fadeOut(0.00000001);
+    $(".form-horizontal").fadeIn(1000);
+}
