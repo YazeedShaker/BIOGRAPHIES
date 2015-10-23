e9b3df8bb1c90809693cc5e8803fa4501d5fd6f5
Yazeed Shaker <yazeed.shaker@outlook.com>
2015-10-23 22:28:46 +0200

    scrollTop and fadeIn the categories

--- Biographies/Biographies/Controller/IntroPage.js
+++ Biographies/Biographies/Controller/IntroPage.js
@@ -1,16 +1,36 @@
-﻿var windowH = $(window).height(),
-    documEle = $(document),
-    slideDownPage = $('.slide-page-down'),
-    btns = $(".btn"),
-    animeSpeed = 500;
+﻿/// <reference path="H:\GitHub\BIOGRAPHIES\Biographies\Biographies\Scripts/jquery-2.1.4.js" />
 
-slideDownPage.css({
-    height: windowH + 'px',
-    top: -windowH + 'px'
+$(document).ready(function () {
+    scrollSections();
+    introButton();
+    pager();
 });
 
-btns.on('click', function (e) {
-    if ($(this).hasClass('open')) {
-        slideDownPage.animate({ 'top': 0 }, animeSpeed)
-    }
-});
+function scrollSections() {
+    $(".page-scroll").click(function (e) {
+        e.preventDefault();
+        var sectionID = e.currentTarget.id + "Section";
+
+        $("html body").animate({
+            scrollTop: $("#" + sectionID).offset().top
+        }, 1000);
+    });
+}
+
+function introButton() {
+    $(".button").click(function (e) {
+        e.preventDefault();
+        $("html body").animate({
+            scrollTop: $("#categoriesSection").offset().top
+        }, 1000);
+    });
+    
+}
+
+function pager() {
+    $(".pager").click(function (e) {
+        e.preventDefault();
+        $(".categories").fadeOut(1500, "swing");
+        $(".categories").fadeIn(1000);
+    });
+}
